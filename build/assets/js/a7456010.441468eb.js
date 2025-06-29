8a7ca83f651ea5c34184cba67cc8d44cef954396
Alfred Nutile <alfrednutile@gmail.com>
2025-06-29 17:03:33 -0400

    yup

---
+++ build/assets/js/a7456010.441468eb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuswork_docs=self.webpackChunkuswork_docs||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'alfred-nutile-inc', // Usually your GitHub org/user name.
   projectName: 'uswork-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
